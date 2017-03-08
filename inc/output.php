fc78488f29851d2d23b533d4b774b879836a41f2
Cade Hockersmith <period1@c126-32.ad.ccsd.net>
2017-03-08 13:21:16 -0800

    GREEN

--- inc/output.php
+++ inc/output.php
@@ -5,7 +5,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Title: " . $row["title"]. " - Release Date: " . $row["releaseDate"]. " Rating:" . $row["rating"]. " Cost:$" . $row["cost"]. " Plaform:" . $row["platform"] . " Genre:" .$row["genre"]. "<br>";
+        echo "<p id='test'>Title: </p>" . $row["title"]. " - Release Date: " . $row["releaseDate"]. " Rating:" . $row["rating"]. " Cost:$" . $row["cost"]. " Plaform:" . $row["platform"] . " Genre:" .$row["genre"]. "<br>";
     }
 } else {
     echo "0 results";
