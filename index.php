7954fc8a08c781b543a2b6f42b2ca5fb46c950d0
Cade Hockersmith <period1@c126-32.ad.ccsd.net>
2017-03-08 11:54:18 -0800

    Fixed Some Stuff

--- index.php
+++ index.php
@@ -16,6 +16,56 @@
 
 </html>
 
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+label{display:inline-block;width:100px;margin-bottom:10px;}
+</style>
+
+
+<title>Add Employee</title>
+</head>
+<body>
+
+    
+<h1>Want to suggest a game?</h1>
+<form method="post" action="">
+<label>Title</label>
+<input type="text" name="title" />
+<br />
+<label>Release Date</label>
+<input type="text" name="releaseDate" />
+<br />
+<label>Rating</label>
+<input type="text" name="rating" />
+<br />
+<label>Cost</label>
+<input type="text" name="cost" />
+<br />
+<label>Platform</label>
+<input type="text" name="platform" />
+<br />
+<label>Genre</label>
+<input type="text" name="genre" />
+
+
+<br />
+<input type="submit" value="Submit Form">
+</form>
+
+
+
+</body>
+</html>
+
+
+
+
+
 <?php
 include("inc/connection.php");
 include("inc/output.php");
